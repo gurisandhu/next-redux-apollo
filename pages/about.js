c864e0259505dfafdf02c509a3a6682e43725467
gurisandhuattarget <guri.sandhu@target.com.au>
2020-01-17 11:40:49 +1100

    about page

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Container from "../components/Container";
+import Header from "../components/Header";
+
+export default () => (
+  <Container>
+    <Header />
+    <article>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia,
+        incidunt fugiat? Animi unde hic consequuntur dolorum illum eos
+        explicabo, quam doloribus atque est velit, quaerat necessitatibus
+        pariatur culpa placeat quos?
+      </p>
+    </article>
+  </Container>
+);
